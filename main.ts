96b562c1b2368dfb68af7bce7144351aa35e3b86
mburdych <70074058+mburdych@users.noreply.github.com>
2020-09-16 17:08:41 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,53 @@
-basic.showIcon(IconNames.Heart)
-SuperBit.Music(SuperBit.enMusic.funk)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
+    } else if (input.buttonIsPressed(Button.B)) {
+        SuperBit.MotorStopAll()
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
+    } else if (input.buttonIsPressed(Button.A)) {
+        SuperBit.MotorStopAll()
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    } else {
+        SuperBit.MotorStopAll()
+    }
+})
