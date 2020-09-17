9d0bc87cb55daf1b2603f983822b269e582b85a3
mburdych <70074058+mburdych@users.noreply.github.com>
2020-09-17 20:31:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+let rychlost = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, rychlost)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, rychlost)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, rychlost)
+        SuperBit.MotorRun(SuperBit.enMotors.M4, rychlost)
+    } else if (input.buttonIsPressed(Button.B)) {
+        SuperBit.MotorStopAll()
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -1 * rychlost)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -1 * rychlost)
+        SuperBit.Servo2(SuperBit.enServo.S1, 270)
+        SuperBit.Servo(SuperBit.enServo.S5, 180)
+        SuperBit.Servo(SuperBit.enServo.S6, 0)
+    } else if (input.buttonIsPressed(Button.A)) {
+        SuperBit.MotorStopAll()
+        SuperBit.MotorRun(SuperBit.enMotors.M1, rychlost)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, rychlost)
+        SuperBit.Servo2(SuperBit.enServo.S1, 0)
+        SuperBit.Servo(SuperBit.enServo.S5, 0)
+        SuperBit.Servo(SuperBit.enServo.S6, 180)
+    } else {
+        SuperBit.Servo(SuperBit.enServo.S5, 90)
+        SuperBit.Servo(SuperBit.enServo.S6, 90)
+        SuperBit.Servo2(SuperBit.enServo.S1, 135)
+        SuperBit.MotorStopAll()
+    }
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
         basic.showLeds(`
@@ -34,20 +62,9 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
-        SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
-    } else if (input.buttonIsPressed(Button.B)) {
-        SuperBit.MotorStopAll()
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
-    } else if (input.buttonIsPressed(Button.A)) {
-        SuperBit.MotorStopAll()
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    if (input.buttonIsPressed(Button.A) || (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB))) {
+        rychlost += 4
     } else {
-        SuperBit.MotorStopAll()
+        rychlost = 0
     }
 })
